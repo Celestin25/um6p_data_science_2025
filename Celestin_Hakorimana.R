cee100b8df6b999c4222d5287550fba25e7e60aa
Celestin25 <celestinhakorimana25@gmail.com>
2026-01-04 13:07:17 +0100

    data science class contest

---
+++ Celestin_Hakorimana.R
@@ -0,0 +1,10 @@
+################################################################################
+# Student Name: Celestin Hakorimana
+# Course: Data Science 1 - Contest Entry
+# Date: Jan 4th, 2026
+# File Name: Celestin_Hakorimana.R
+# Description: Analysis of Class Promo06 Demographics (Nationality & Gender)
+################################################################################
+# Load necessary library
+if (!require("tidyverse")) install.packages("tidyverse")
+library(tidyverse)
